5c3c34fce0e99ea7bef91ec35947fb996fb48a75
Patricio Molina <patriciomolina@gmail.com>
2013-09-18 18:11:46 -0300

    Initial files

---
+++ serve.py
@@ -0,0 +1,9 @@
+from bottle import route, run, response
+from rbg import generate_bs
+
+@route('/')
+def home():
+	response.content_type = 'application/json'
+	return generate_bs()
+
+run(host='localhost', port=8080, debug=True)
