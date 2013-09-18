393f513482be2c6eeaa30727a35ff97e623eb01f
Patricio Molina <patriciomolina@gmail.com>
2013-09-18 18:29:52 -0300

    "pep8ifying" files

--- serve.py
+++ serve.py
@@ -1,9 +1,10 @@
 from bottle import route, run, response
 from rbg import generate_bs
 
+
 @route('/')
 def home():
-	response.content_type = 'application/json'
-	return generate_bs()
+    response.content_type = 'application/json'
+    return generate_bs()
 
 run(host='localhost', port=8080, debug=True)
